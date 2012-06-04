49b06960ddb571a3a71661240d4a7e1194fb9075
David Dexer <david@codesly.com>
2012-06-04 16:29:38 -0700

    Roll back to master

--- system/expressionengine/third_party/brilliant_retail/mod.brilliant_retail.php
+++ system/expressionengine/third_party/brilliant_retail/mod.brilliant_retail.php
@@ -1139,23 +1139,6 @@ class Brilliant_retail extends Brilliant_retail_core{
 			return $this->return_data;
 		}
 
-		/**
-		* Check to see if a discount exits
-		* 
-		* @access	public
-		* @param	void
-		* @return	boolean 
-		*/
-			
-		public function cart_has_discount() {
-	        if(isset($_SESSION["discount"])){
-	            if($_SESSION["discount"]["code"] != ''){
-	                return ($this->_get_cart_discount() > 0);
-	            }
-	        }
-	        return FALSE; 
-	    }
-	 
 		function cart_total()
 		{
 			// Simple function that adds the currency marker to the 
