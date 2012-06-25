3d64b7a9e9ba56e10b7b7e760c95b979998f7990
David Dexter <david@brilliant2.com>
2012-06-25 15:02:08 -0700

    adding google checkout gateway shell file

---
+++ system/expressionengine/third_party/brilliant_retail/core/gateway/gateway.google_checkout.php
@@ -0,0 +1,103 @@
+<?php
+/************************************************************/
+/*	BrilliantRetail 										*/
+/*															*/
+/*	@package	BrilliantRetail								*/
+/*	@Author		David Dexter  								*/
+/* 	@copyright	Copyright (c) 2010-2012						*/
+/* 	@license	http://brilliantretail.com/license.html		*/
+/* 	@link		http://brilliantretail.com 					*/
+/*															*/
+/************************************************************/
+/* NOTICE													*/
+/*															*/
+/* THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF 	*/
+/* ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED	*/
+/* TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A 		*/
+/* PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT 		*/
+/* SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY */
+/* CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION	*/
+/* OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR 	*/
+/* IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER 		*/
+/* DEALINGS IN THE SOFTWARE. 								*/	
+/************************************************************/
+
+class Gateway_google_checkout extends Brilliant_retail_gateway {
+	public $title 		= 'Google Checkout';
+	public $label 		= 'Google Checkout';
+	public $descr 		= 'Accept payments via Google Checkout';
+	public $enabled 	= true;
+	public $version 	= '1.0';
+	public $ipn_enabled = true;
+	public $osc_enabled = true;
+	public $cart_button = false;
+	
+	public function process($data,$config){
+		
+			// Process only builds a process shell and sets order status = 1
+			
+			// Set the transaction details into 
+			// a serialized array for posting to
+			// the order
+				$details = array(
+									"Method" => "PayPal Standard" 
+								);
+
+			// Return the trans details 
+				$trans = array(
+									'status' => -1, 
+									'transaction_id' => $data["transaction_id"],
+									'payment_card' => "",
+									'payment_type' => 'PayPal Standard', 
+									'amount' => $data["order_total"],
+									'details' => serialize($details), 
+									'approval' => "" 
+								);
+			return $trans;
+		}
+
+	// Start IPN Call
+		public function start_ipn($data,$config)
+		{
+		}
+	
+	// Process IPN Calls 
+		public function gateway_ipn($config){
+		}
+	
+	// Create a inputs for the checkout form
+		public function form()
+		{
+		}
+	
+	// Install the gateway
+		public function install($config_id){
+			$data = array();
+			$data[] = array(
+							'config_id' => $config_id, 
+							'label'	 	=> 'Business E-mail', 
+							'code'		=> 'email', 
+							'type' 		=> 'text',
+							'required' 	=> true,
+							'sort' 		=> 1
+							);
+			$data[] = array(
+							'config_id' => $config_id, 
+							'label'	 	=> 'Sandbox Mode', 
+							'code' 		=> 'sandbox',
+							'type' 		=> 'dropdown', 
+							'options' 	=> 'TRUE:True|FALSE:False (Transactions are Live)',
+							'value' 	=> 'TRUE',
+							'sort' 		=> 2
+							);	
+			foreach($data as $d){
+				$this->EE->db->insert('br_config_data',$d);
+			}				
+			return true;
+	}
+
+	// Remove the gateway
+		public function remove($config_id){
+			return true;		
+		}
+}
