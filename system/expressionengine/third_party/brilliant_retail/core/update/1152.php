946a868197fe49488a02d406252ca0b91103ebd0
Kevin Cupp <kevin.cupp@gmail.com>
2012-08-30 12:47:05 -0400

    Fixing 1.1.5.2 updater for large databases

--- system/expressionengine/third_party/brilliant_retail/core/update/1152.php
+++ system/expressionengine/third_party/brilliant_retail/core/update/1152.php
@@ -22,32 +22,41 @@
 /* DEALINGS IN THE SOFTWARE. 								*/	
 /************************************************************/
 
-	// GET A COLLECTION OF THE order download records that are 0 
-	$stmt="SELECT * FROM exp_br_order_download WHERE member_id = 0";
-	$qry = $this->EE->db->query($stmt);
-	$order = array();
-	foreach($qry->result_array() as $rst)
+	$processed = 0;
+	$count = $this->EE->db->count_all('exp_br_order_download');
+	
+	// Work in batches of 100
+	while ($processed < $count)
 	{
-		if(isset($order[$rst["order_id"]])){
-			$member_id = $order[$rst["order_id"]];
-		}else{
-			$stmt = "SELECT member_id FROM exp_br_order WHERE order_id = ".$rst["order_id"];
-			$query = $this->EE->db->query($stmt);
-			$row = $query->result_array();
-			// Shouldn't be any rows in here without order_ids but lets 
-			// add this just so we dont crash and burn if there are. 
-				if(isset($row[0]))
-				{
-					$member_id = $row[0]["member_id"];
-				}else{
-					$member_id = 0;
-				}
+		$processed += 100;
+		
+		// GET A COLLECTION OF THE order download records that are 0 
+		$stmt="SELECT * FROM exp_br_order_download WHERE member_id = 0 LIMIT 100";
+		$qry = $this->EE->db->query($stmt);
+		$order = array();
+		foreach($qry->result_array() as $rst)
+		{
+			if(isset($order[$rst["order_id"]])){
+				$member_id = $order[$rst["order_id"]];
+			}else{
+				$stmt = "SELECT member_id FROM exp_br_order WHERE order_id = ".$rst["order_id"];
+				$query = $this->EE->db->query($stmt);
+				$row = $query->result_array();
+				// Shouldn't be any rows in here without order_ids but lets 
+				// add this just so we dont crash and burn if there are. 
+					if(isset($row[0]))
+					{
+						$member_id = $row[0]["member_id"];
+					}else{
+						$member_id = 0;
+					}
+			}
+			// Build the update statement
+				$stmt = "UPDATE exp_br_order_download set member_id = ".$member_id." WHERE order_download_id = ".$rst["order_download_id"];
+				$this->EE->db->query($stmt);
+			// Save a query if we are doing multiple 
+			// downloads per order
+				$order[$rst["order_id"]]=$member_id;
 		}
-		// Build the update statement
-			$stmt = "UPDATE exp_br_order_download set member_id = ".$member_id." WHERE order_download_id = ".$rst["order_download_id"];
-			$this->EE->db->query($stmt);
-		// Save a query if we are doing multiple 
-		// downloads per order
-			$order[$rst["order_id"]]=$member_id;
 	}
 	
