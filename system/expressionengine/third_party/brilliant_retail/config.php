9069c1c09d3873ff84cca576544cf186cefbb021
David Dexer <david@codesly.com>
2013-11-09 11:06:20 -0800

    update to dev release 1.4.1.9 and added config location notice

--- system/expressionengine/third_party/brilliant_retail/config.php
+++ system/expressionengine/third_party/brilliant_retail/config.php
@@ -1,8 +1,18 @@
 <?php
-
+/********************************************************************************/
+/* IMPORTANT NOTICE																*/
+/* 																				*/
+/* This file should not be edited. It is a core package file and will be 		*/
+/* overwritten with updates. 													*/
+/* 																				*/
+/* If you need to add your own config variables they should be added 			*/
+/* to the /system/expressionengine/config/config.php file. 						*/
+/* 																				*/
+/********************************************************************************/
+ 
 if ( ! defined('BR_VERSION'))
 {
-	define('BR_VERSION','1.4.1.8');
+	define('BR_VERSION','1.4.1.9');
 }
 
 $config['name']			=	'BrilliantRetail';
