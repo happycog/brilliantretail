188833a6481c16304c30c547444fe9426493f240
David Dexer <david@codesly.com>
2013-12-18 13:09:13 -0800

    updating to 1512 for dev release

--- system/expressionengine/third_party/brilliant_retail/config.php
+++ system/expressionengine/third_party/brilliant_retail/config.php
@@ -11,7 +11,7 @@
 /********************************************************************************/
 
 if ( ! defined('BR_VERSION')) {
-    define('BR_VERSION','1.5.1.1');
+    define('BR_VERSION','1.5.1.2');
 }
 
 $config['name']			=	'BrilliantRetail';
