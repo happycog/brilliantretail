d94eb52afb682bd88075e55d5eb6efead73762a1
David Dexer <david@codesly.com>
2014-02-05 09:17:42 -0800

    tagging 1.5.1.6

--- system/expressionengine/third_party/brilliant_retail/config.php
+++ system/expressionengine/third_party/brilliant_retail/config.php
@@ -11,7 +11,7 @@
 /********************************************************************************/
 
 if ( ! defined('BR_VERSION')) {
-    define('BR_VERSION','1.5.1.5');
+    define('BR_VERSION','1.5.1.6');
 }
 
 $config['name']			=	'BrilliantRetail';
