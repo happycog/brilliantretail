e8b90fe6316b8c0c52698ef95008b9f94f5d20a6
David Dexer <david@codesly.com>
2014-02-20 13:31:48 -0800

    adding update file to repo 1518

---
+++ system/expressionengine/third_party/brilliant_retail/core/update/1518.php
@@ -0,0 +1,66 @@
+<?php if ( ! defined('BASEPATH')) exit('No direct script access allowed');
+/************************************************************/
+/*	BrilliantRetail 										*/
+/*															*/
+/*	@package	BrilliantRetail								*/
+/*	@Author		David Dexter  								*/
+/* 	@copyright	Copyright (c) 2010-2013						*/
+/* 	@license	http://brilliantretail.com/license.html		*/
+/* 	@link		http://brilliantretail.com 					*/
+/*															*/
+/************************************************************/
+/* NOTICE													*/
+/*															*/
+/* THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF 	*/
+/* ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED	*/
+/* TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A 		*/
+/* PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT 		*/
+/* SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY */
+/* CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION	*/
+/* OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR 	*/
+/* IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER 		*/
+/* DEALINGS IN THE SOFTWARE. 								*/	
+/************************************************************/
+
+    // Set the state title to the country name for countries sans states
+        $sql[] = "INSERT INTO exp_br_state (zone_id,code,title,enabled) VALUES (8,'AI','Anguilla',1)";
+        $sql[] = "INSERT INTO exp_br_state (zone_id,code,title,enabled) VALUES (9,'AQ','Antarctica',1)";
+        $sql[] = "INSERT INTO exp_br_state (zone_id,code,title,enabled) VALUES (13,'AW','Aruba',1)";
+        $sql[] = "INSERT INTO exp_br_state (zone_id,code,title,enabled) VALUES (30,'BV','Bouvet Island',1)";
+        $sql[] = "INSERT INTO exp_br_state (zone_id,code,title,enabled) VALUES (32,'IO','British Indian Ocean Territory',1)";
+        $sql[] = "INSERT INTO exp_br_state (zone_id,code,title,enabled) VALUES (33,'VG','British Virgin Islands',1)";
+        $sql[] = "INSERT INTO exp_br_state (zone_id,code,title,enabled) VALUES (42,'KY','Cayman Islands',1)";
+        $sql[] = "INSERT INTO exp_br_state (zone_id,code,title,enabled) VALUES (47,'CX','Christmas Island',1)";
+        $sql[] = "INSERT INTO exp_br_state (zone_id,code,title,enabled) VALUES (48,'CC','Cocos [Keeling] Islands',1)";
+        $sql[] = "INSERT INTO exp_br_state (zone_id,code,title,enabled) VALUES (50,'KM','Comoros',1)";
+        $sql[] = "INSERT INTO exp_br_state (zone_id,code,title,enabled) VALUES (53,'CK','Cook Islands',1)";
+        $sql[] = "INSERT INTO exp_br_state (zone_id,code,title,enabled) VALUES (71,'FK','Falkland Islands',1)";
+        $sql[] = "INSERT INTO exp_br_state (zone_id,code,title,enabled) VALUES (72,'FO','Faroe Islands',1)";
+        $sql[] = "INSERT INTO exp_br_state (zone_id,code,title,enabled) VALUES (76,'GF','French Guiana',1)";
+        $sql[] = "INSERT INTO exp_br_state (zone_id,code,title,enabled) VALUES (84,'GI','Gibraltar',1)";
+        $sql[] = "INSERT INTO exp_br_state (zone_id,code,title,enabled) VALUES (96,'HM','Heard Island and McDonald Islands',1)";
+        $sql[] = "INSERT INTO exp_br_state (zone_id,code,title,enabled) VALUES (124,'LI','Liechtenstein',1)";
+        $sql[] = "INSERT INTO exp_br_state (zone_id,code,title,enabled) VALUES (127,'MO','Macau SAR China',1)";
+        $sql[] = "INSERT INTO exp_br_state (zone_id,code,title,enabled) VALUES (134,'MT','Malta',1)";
+        $sql[] = "INSERT INTO exp_br_state (zone_id,code,title,enabled) VALUES (136,'MQ','Martinique',1)";
+        $sql[] = "INSERT INTO exp_br_state (zone_id,code,title,enabled) VALUES (154,'AN','Netherlands Antilles',1)";
+        $sql[] = "INSERT INTO exp_br_state (zone_id,code,title,enabled) VALUES (160,'NU','Niue',1)";
+        $sql[] = "INSERT INTO exp_br_state (zone_id,code,title,enabled) VALUES (161,'NF','Norfolk Island',1)";
+        $sql[] = "INSERT INTO exp_br_state (zone_id,code,title,enabled) VALUES (163,'MP','Northern Mariana Islands',1)";
+        $sql[] = "INSERT INTO exp_br_state (zone_id,code,title,enabled) VALUES (168,'PS','Palestinian Territories',1)";
+        $sql[] = "INSERT INTO exp_br_state (zone_id,code,title,enabled) VALUES (174,'PN','Pitcairn Islands',1)";
+        $sql[] = "INSERT INTO exp_br_state (zone_id,code,title,enabled) VALUES (182,'RE','Reunion',1)";
+        $sql[] = "INSERT INTO exp_br_state (zone_id,code,title,enabled) VALUES (188,'PM','Saint Pierre and Miquelon',1)";
+        $sql[] = "INSERT INTO exp_br_state (zone_id,code,title,enabled) VALUES (189,'VC','Saint Vincent and the Grenadines',1)";
+        $sql[] = "INSERT INTO exp_br_state (zone_id,code,title,enabled) VALUES (214,'ST','Sao Tome and Principe',1)";
+        $sql[] = "INSERT INTO exp_br_state (zone_id,code,title,enabled) VALUES (197,'SG','Singapore',1)";
+        $sql[] = "INSERT INTO exp_br_state (zone_id,code,title,enabled) VALUES (203,'GS','South Georgia and the South Sandwich Islands',1)";
+        $sql[] = "INSERT INTO exp_br_state (zone_id,code,title,enabled) VALUES (209,'SJ','Svalbard and Jan Mayen',1)";
+        $sql[] = "INSERT INTO exp_br_state (zone_id,code,title,enabled) VALUES (221,'TK','Tokelau',1)";
+        $sql[] = "INSERT INTO exp_br_state (zone_id,code,title,enabled) VALUES (222,'TO','Tonga',1)";
+        $sql[] = "INSERT INTO exp_br_state (zone_id,code,title,enabled) VALUES (227,'TC','Turks and Caicos Islands',1)";
+        $sql[] = "INSERT INTO exp_br_state (zone_id,code,title,enabled) VALUES (228,'TV','Tuvalu',1)";
+        $sql[] = "INSERT INTO exp_br_state (zone_id,code,title,enabled) VALUES (237,'VU','Vanuatu',1)";
+        $sql[] = "INSERT INTO exp_br_state (zone_id,code,title,enabled) VALUES (238,'VA','Vatican City',1)";
+        $sql[] = "INSERT INTO exp_br_state (zone_id,code,title,enabled) VALUES (241,'WF','Wallis and Futuna',1)";
+        $sql[] = "INSERT INTO exp_br_state (zone_id,code,title,enabled) VALUES (242,'EH','Western Sahara',1)";
