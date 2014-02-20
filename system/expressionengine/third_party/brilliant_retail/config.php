73b1242c1551b040ca166a95ba72d693eb495980
David Dexer <david@codesly.com>
2014-02-20 13:28:18 -0800

    adding matching state entries for countries sans states

--- system/expressionengine/third_party/brilliant_retail/config.php
+++ system/expressionengine/third_party/brilliant_retail/config.php
@@ -11,7 +11,7 @@
 /********************************************************************************/
 
 if ( ! defined('BR_VERSION')) {
-    define('BR_VERSION','1.5.1.7');
+    define('BR_VERSION','1.5.1.8');
 }
 
 $config['name']			=	'BrilliantRetail';
