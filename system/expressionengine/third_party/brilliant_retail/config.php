5b521eaddf0e6002280583603d80d842c345291b
David Dexer <david@codesly.com>
2014-08-11 09:42:10 -0700

    updating to 1.6.3 and logging changes in readme

--- system/expressionengine/third_party/brilliant_retail/config.php
+++ system/expressionengine/third_party/brilliant_retail/config.php
@@ -11,7 +11,7 @@
 /********************************************************************************/
 
 if ( ! defined('BR_VERSION')) {
-    define('BR_VERSION','1.6.2.4');
+    define('BR_VERSION','1.6.3.0');
 }
 
 $config['name']			=	'BrilliantRetail';
