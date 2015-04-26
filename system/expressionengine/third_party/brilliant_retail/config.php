8ecb174fd724dad55e8910b9dd7c53e27437d1c2
David Dexer <david@codesly.com>
2015-04-26 15:24:37 -0700

    updating changelog and version number

--- system/expressionengine/third_party/brilliant_retail/config.php
+++ system/expressionengine/third_party/brilliant_retail/config.php
@@ -11,7 +11,7 @@
 /********************************************************************************/
 
 if ( ! defined('BR_VERSION')) {
-    define('BR_VERSION','1.7.1.0');
+    define('BR_VERSION','1.8.0');
 }
 
 $config['name']			=	'BrilliantRetail';
